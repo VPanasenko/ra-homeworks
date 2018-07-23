7fe79cc497d6565b8e9444b1388a5394922718b3
VPanasenko <panasenko.victor@gmail.com>
2018-07-23 12:22:20 +0300

    Hometask #4
    Домашнее задание №4 сделано.

--- state/filter/js/App.js
+++ state/filter/js/App.js
@@ -1,11 +1,43 @@
 'use strict'
 
-const App = props => (
-  <div>
-    <Toolbar
-      filters={props.filters}
-      selected={'All'}
-      onSelectFilter={(filter) => console.log(filter)} />
-    <Portfolio projects={props.projects} />
-  </div>
-);
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      filter: 'All',
+    }
+  }
+
+  changeFilter = (f) => {
+    this.setState({
+      filter: f,
+    })
+  }
+
+  filterProjects = () => {
+    let filteredProjects = this.props.projects.slice();
+
+    switch(this.state.filter)
+    {
+      case 'All':
+        return filteredProjects;
+      default:
+        return filteredProjects.filter(p=>p.category === this.state.filter)
+    }
+  }
+
+
+  render(){
+    
+
+    return (
+      <div>
+        <Toolbar
+          filters={this.props.filters}
+          selected={this.state.filter}
+          onSelectFilter={(filter) => this.changeFilter(filter)} />
+        <Portfolio projects={this.filterProjects()} />
+      </div>
+    )
+  }
+}
--- state/layouts/js/App.js
+++ state/layouts/js/App.js
@@ -4,19 +4,39 @@ const VIEW_LIST = "view_list";
 const VIEW_MODULE = "view_module";
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      icon: VIEW_LIST,
+      displayAsList: true,
+    }
+  }
+
+  switchView = () => {
+    if (this.state.icon === VIEW_LIST){
+      this.setState({icon: VIEW_MODULE, displayAsList: false});
+    } 
+    else{
+      this.setState({icon: VIEW_LIST, displayAsList: true});
+    }
+  }
+
+
   render() {
     return (
       <div>
         <div className="toolbar">
           <IconSwitch
-            icon={VIEW_MODULE}
-            onSwitch={() => console.log("сменился тип вывода")} />
+            icon={this.state.icon}
+            onSwitch={this.switchView} />
         </div>
-        {this.renderLayout(true)}
+        {this.renderLayout(this.state.displayAsList)}
       </div>
     );
   }
 
+  //Можно и ниже переписать с cardView на this.state.displayAsList, 
+  //чтобы, например, уменьшить число передаваемых атрибутов у методов renderLayout и getShopItems. 
   renderLayout(cardView) {
     if (cardView) {
       return (
