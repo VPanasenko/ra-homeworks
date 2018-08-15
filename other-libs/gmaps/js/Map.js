0665863b68c2e7652bfc73982411c5d8288501cb
Victor Panasenko <victor.panasenko@outlook.com>
2018-08-15 18:49:33 +0300

    Hometask #4.2 Part 1
    Task 1 in progress.

--- other-libs/gmaps/js/Map.js
+++ other-libs/gmaps/js/Map.js
@@ -1,10 +1,38 @@
 class Map extends React.Component {
-  render() {
-    return (
-      <div>
-        Карта
-        {this.props.points.map(office => <p>{JSON.stringify(office)}</p>)}
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      points: []
+    };
   }
+
+  map = null;
+  mapRef = null;
+
+  componentWillMount() {
+    if (this.props.points) {
+      this.setState({
+        points: this.props.points
+      });
+    }
+  }
+
+  componentDidMount() {
+    console.log(this.mapRef);
+    this.map = new google.maps.Map(this.mapRef, {
+      center: {
+        lat: -34.397,
+        lng: 150.644
+      },
+      zoom: 8
+    });
+  }
+
+  componentWillReceiveProps() {}
+
+  render() {
+    return <div ref={el => (this.mapRef = el)} />;
+  }
+
+  componentWillUnmount() {}
 }
